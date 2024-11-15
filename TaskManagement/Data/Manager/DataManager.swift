a28e5da3c1f928dd9f41d9c54802c702542de0b5
byhabiboff <byhabiboff@gmail.com>
2024-11-15 14:22:35 +0400

    Added new method

---
+++ TaskManagement/Data/Manager/DataManager.swift
@@ -0,0 +1,17 @@
+//
+//  DataManager.swift
+//  TaskManagement
+//
+//  Created by Nahit Habibov on 15.11.24.
+//
+
+import Foundation
+
+// Add cutom error metod
+
+class DataManager {
+    
+    func loadJson<T: Decodable>(filenName: String, completion: @escaping (Result<T, Error>) -> Void) {
+        
+    }
+}
